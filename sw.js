52168683795352ff78e15ed410f07388ec881ce5
ziwhs4826-stack <ziwhs4826@gmail.com>
2026-05-08 09:37:40 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "pick-terminal-v1";
+const cacheName = "pick-terminal-v2";
 const assets = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (event) => {
