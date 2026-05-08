4f7aa02c8d7414cd2ab0855d9f5e6ee21fbb9c67
ziwhs4826-stack <ziwhs4826@gmail.com>
2026-05-08 09:21:09 +0900

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const cacheName = "pick-terminal-v1";
+const assets = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
